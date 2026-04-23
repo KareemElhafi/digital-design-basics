0b8374f57077f92c5a88102f92130c3a9dd78b69
Kareem Elhafi <kareemelhafi@gmail.com>
2026-04-23 18:19:37 +0200

    tb(full_subtractor): add stimulus generator

---
+++ verification/full_subtractor_tb/generator.sv
@@ -0,0 +1,21 @@
+class fs_generator;
+
+    mailbox gen2drv;
+    int num_tests;
+
+    function new(mailbox gen2drv, int num_tests = 20);
+        this.gen2drv = gen2drv;
+        this.num_tests = num_tests;
+    endfunction
+
+    task run();
+        fs_seq_item tr;
+
+        repeat (num_tests) begin
+            tr = new();
+            assert(tr.randomize()) else $fatal("Randomization Failed");
+            gen2drv.put(tr);
+        end
+    endtask
+
+endclass
